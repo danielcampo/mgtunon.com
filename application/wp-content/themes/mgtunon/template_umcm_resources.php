b99d93d8dad73afad7a732e472bfbfc7acce15f0
Daniel Campo <accounts@epiksolcreative.com>
2011-08-25 11:24:59 -0400

    Added redirects for old UMCM pages and added temporary UMCM resources template.

---
+++ application/wp-content/themes/mgtunon/template_umcm_resources.php
@@ -0,0 +1,344 @@
+<?php
+/*
+	Template Name: UMCM Resources
+*/
+?>
+
+<?php get_header(); ?>
+
+<script>
+$( document ).ready( function () {
+	$('.teenJobs_spanish').toggle(
+		
+		function() {
+		$('.teenJobs_spanish_letter').slideDown('slow');
+		},
+		
+		function() {
+		$('.teenJobs_spanish_letter').slideUp('slow');
+		}
+	);
+	
+	$('.teenJobs_english').toggle(
+		
+		function() {
+		$('.teenJobs_english_letter').slideDown('slow');
+		},
+		
+		function() {
+		$('.teenJobs_english_letter').slideUp('slow');
+		}
+	);	
+});
+</script>
+
+		<div id="page_main" role="main" class="grid_16<?php mgt_ptype(); ?>">
+			
+			<div id="page_main_left" class="grid_10 alpha omega">
+				
+				<section id="page_content">
+				
+					<div class="resource">
+
+						<a name="082411"></a>
+						<h1>Ayuda Para Inmigrantes Buscando Empleo* - August 24, 2011</h1>
+						
+						<p>
+							<strong><em>Referencia:</em></strong><br />
+							<em>Sabemos que el mercado laboral est&#225; muy competitivo &#8211; las &#250;ltimas estad&#237;sticas indican que la tasa de desempleo nacionalmente esta en 9.2% - y var&#237;a por para estado (en la Florida la tasa de desempleo estatal esta en 10.6%).  Ya, en s&#237;, el proceso de buscar empleo es dif&#237;cil para cualquier persona &#8211; pero es m&#225;s dif&#237;cil, inclusive, para los inmigrantes que quiz&#225;s no tengan las mismas experiencias de c&#243;mo manejar este proceso, o como aplicar para puestos de trabajo, ni siquiera hablan el idioma.</em>
+						</p>
+						
+						<strong><em>Consejos:</em></strong>
+						<ul>
+							<li>Obviamente uno tiene que tomar en consideraci&#243;n las visas y permisos necesarios para poder trabajar en este pa&#237;s &#8211; pero eso no es un tema para este segmento y siempre es mejor dejar que un profesional legal en el &#225;rea de inmigraci&#243;n hable sobre esos requerimientos&#160; - esa no es mi &#225;rea &#8211; en ves nos vamos a concentrar en sugerencias que un inmigrante debe considerar para buscar empleo de una forma pr&#225;ctica y real&#8230;</li>
+						</ul>
+
+						<ul>
+							<li><strong>Aprenda ingles</strong>.&#160; <em>Yo entiendo que muchas personas, especialmente las personas mayores, no tienen la motivaci&#243;n para aprender ingl&#233;s, pero uno no se puede imaginar cuanto ayuda en el proceso de buscar empleo y cuantas puertas pueda abrir.&#160; No pretendo que aprendan a hablar ingl&#233;s perfectamente bien, pero si hay ciertas frases b&#225;sicas que uno debe dominar.</em></li>
+						<ol>
+							<li>El gobierno tiene un servicio gratis para aprender ingles por internet: <a href="http://www.usalearns.org/">www.usalearns.org</a></li>
+						</ol>
+							<li><strong>Analizar el trabajo que desea en el contexto de las oportunidades que existen en el area donde vive.</strong>&#160; Esto aplica tanto para profesionales como para personas que no tienen una carrera formal.&#160; Oportunidades var&#237;an por ciudad.&#160; En Miami, por ejemplo las industrias crecientes incluyen:</li>
+						<ol class="list-alpha">
+							<li><strong>Bancos y Finanzas:</strong>&#160;&#160;Miami es un centro importante bancos dom&#233;sticos e internacionales.&#160;&#160;Muchos de los Bancos internacionales tienen sus oficinas de Latinoam&#233;rica en Miami.&#160;&#160;Inclusive existen servicios de reclutamiento que se dedican exclusivamente a la industria financiera.&#160; (<a href="http://www.Nasonsearch.com/">www.Nasonsearch.com</a>)</li>
+							<li><strong>Hospitalidad:</strong>&#160;&#160;Miami es una meca tur&#237;stica.&#160;&#160;Oportunidades existen para todos los niveles de empleo tanto en los hoteles como los restaurantes y tambi&#233;n las compa&#241;&#237;as que proveen servicios a estos negocios.</li>
+							<li><strong>Informaci&#243;n Tecnol&#243;gica</strong>:&#160;La industria tecnol&#243;gica de Miami est&#225; creciendo.&#160;&#160;&#160;&#160;<a href="http://www.dice.com/">www.dice.com</a>.</li>
+						</ol>
+							<li>
+								<strong>Encuentre un mentor</strong>:&#160; Hable con amistades y familiares para encontrar un mentor de confianza que le pueda ayudar en el proceso.&#160; Tambi&#233;n puede visitar <a href="http://www.score.org/en-espanol">www.score.org/en-espanol</a>. &#160;SCORE es una asociaci&#243;n sin fines de lucro a nivel nacional dedicada al entrenamiento de empresarios, y a ayudar a las peque&#241;as empresas a su lanzamiento, crecimiento y alcanzar el &#233;xito.</li>
+							
+							<li><strong>Utilice sus redes de contactos sociales</strong></li>
+								<ol class="list-alpha">
+									<li>Twitter</li>
+									<li>Facebook</li>
+									<li>El Nuevo <a href="http://Herald.com/">Herald.com</a></li>
+									<li>Redes de contactos personales</li>
+								</ol>
+							
+							<li><strong>Prepare su resume (Curriculum Vitae)&#160; (link them to our resume service&#8230;)</strong></li>
+								<ol class="list-alpha">
+									<li>Carta de presentaci&#243;n</li>
+									<li>Objetivo</li>
+									<li>Aptitudes</li>
+									<li>Experiencia Profesional</li>
+									<li>Formaci&#243;n Acad&#233;mica</li>
+									<li>Premios y Reconocimientos</li>
+									<li>Formato</li>
+									<li>Uso correcto del ingl&#233;s y la gram&#225;tica</li>
+									<li>Comunicaci&#243;n clara y efectiva</li>
+								</ol>
+							
+							<li>
+								<strong>Prep&#225;rese para la entrevista (link them to the segment where we talk about this)</strong>
+							</li>
+								<ol class="list-alpha">
+									<li>Antes de la entrevista</li>
+									<li>Durante la entrevista</li>
+									<li>Despu&#233;s de la entrevista</li>
+								</ol>
+						</ul>
+
+					</div>
+					<!-- END Resource 08.24.11 -->	
+
+					<div class="resource">
+
+						<a name="061611"></a>
+						<h1>Summer Jobs for Teens - June 16, 2011</h1>
+
+						<ul class="hotResourcesList">
+							<li>
+								Sample Intro Letter/Email:
+								<ul>
+									<li>
+										<a class="teenJobs_spanish" href="#">Spanish</a>
+										<div class="teenJobs_spanish_letter">
+										<p>Hola familia y amigos,</p>
+										
+										<p>
+											Espero que se encuentren bien y que est&#233;n disfrutando del verano.  Yo acabo de terminar mi (tercer) a&#241;o de escuela secundaria y espero que el pr&#243;ximo a&#241;o escolar sea excelente.  El motivo de este correo es para hacerles saber que estoy buscando una oportunidad para trabajar este verano.  Mi objetivo es obtener el conocimiento del mundo de negocios a trav&#233;s de un trabajo de tiempo parcial o internado.
+										</p>
+											
+										<p>	
+											Creo que mis habilidades de comunicaci&#243;n, mi aptitud de servicio al cliente y &#233;tica de trabajo me ayudar&#237;an a contribuir de una forma productiva y positiva a su organizaci&#243;n.
+										</p>
+
+										<p>
+											Gracias por su consideraci&#243;n.  Si necesita cualquier informaci&#243;n adicional estoy disponible para reunirme con usted personalmente.  Espero con inter&#233;s escuchar de usted.
+										</p>
+
+											
+										<p>Muchas gracias,<br />
+											(Nombre&#8230;)<br />
+											(Numero de tel&#233;fono / correo electr&#243;nico)
+										</p>
+										
+										
+										<a href="/downloads/umcm/061611/teenJobs_sampleLetter_spanish.pdf">Download PDF</a>
+										</div>
+										<!-- END Spanish -->
+															
+									</li>
+									<li>
+										<a class="teenJobs_english" href="#">English</a>
+									
+										<div class="teenJobs_english_letter">
+										<p>	
+											Hello family and friends,
+										</p>
+
+										<p>
+											I hope that you are doing well, and are enjoying the summer.  I just completed my Junior year, and am looking forward to a terrific Senior year.  The reason for this email is to let you know that I am looking for a summer job.  My objective is to obtain knowledge of the day-to-day work of a business through a part time job, summer internship and/or job shadowing experience.
+										</p>
+										
+										<p>
+											 I believe that my communication skills, customer service abilities, and positive work ethic would make me an asset to your organization.
+										</p>
+
+										<p>
+											Thank you for your consideration.  If you require any further information, I am available to meet with you personally. I look forward to hearing from you.
+										</p>
+
+										<p>
+											Many thanks,<br />
+											(Name&#8230;)<br />
+											(phone number / email address)
+										</p>
+										
+										<a href="/downloads/umcm/061611/teenJobs_sampleLetter_english.pdf">Download PDF</a>
+										</div>
+										<!-- END English -->
+										
+									</li>
+
+								</ul>
+							</li>
+							<li>
+								<a href="http://www.myfloridalicense.com/dbpr/reg/childlabor/ChildLaborParent.html">Florida Labor Laws</a>
+							</li>
+
+						</ul>
+					</div>
+					<!-- END Resource 06.16.11 -->
+					
+					<div class="resource">
+
+						<a name="051211"></a>
+						<h1>Stress Management - May 12, 2011</h1>
+
+						<ul class="hotResourcesList">
+							<li>
+								<a href="http://www.helpguide.org/mental/stress_management_relief_coping.htm">Stress Management</a>
+							</li>
+							<li>
+								<a href="http://www.saludalia.com/docs/Salud/web_saludalia/vivir_sano/doc/psicologia/doc/doc_trabajo_estres.htm">&#191;Por qu&#233; el trabajo me produce estr&#233;s?</a>
+							</li>
+							<li>
+								<a href="http://www.mayoclinic.com/health/coping-with-stress/SR00030/NSECTIONGROUP=2">Tips for Coping with Stress at Work</a>
+							</li>
+
+						</ul>
+					</div>
+					<!-- END Resource 05.12.11 -->	
+
+					<div class="resource">
+					
+						<a name="041411"></a>	
+						<h1>Lenguaje Corporal - April 14, 2011</h1>
+					
+						<ul>
+							<li><a href="http://finance.yahoo.com/career-work/article/102425/Body_Language:_A_Key_to_Success_in_the_Workplace">Body Language: A Key to Success in the Workplace</a></li>
+
+							<li><a href="http://www.people-communicating.com/lenguaje-corporal.html">Lenguaje Corporal en el Trabajo</a></li>
+
+							<li><a href="http://www.buscarempleo.es/opinion/el-lenguaje-corporal-en-el-trabajo.html">El lenguaje corporal en el trabajo</a></li>
+						</ul>
+					</div>
+					<!-- END Resource 04.13.11 -->
+
+					<div class="resource">
+						
+
+						<a name="031711"></a>
+						<h1>De Regreso Al Colegio - March 17, 2011</h1>
+					
+						<p><strong>Sabemos que muchas personas est&#225;n pensando en regresar al colegio despu&#233;s de muchos a&#241;os de estar trabajando.  Hay aquellos que nunca terminaron su educaci&#243;n formal y siempre se quedaron con las ganas.  Otros buscan un aumento de sueldo o una promoci&#243;n.  Y aun otros est&#225;n en busca de una nueva carrera la cual requiere nuevos talentos que es capaz que puedan encontrar en la escuela.  Hoy voy a enfocarme en una pregunta crucial, &#191;Le ayudar&#237;a a mi carrera o mi negocio si yo regresar&#237;a al colegio?</strong></p>
+
+						<p><strong>Regresar al colegio es una decisi&#243;n compleja y seria.  &#191;C&#243;mo determina uno si el regresar al colegio es lo correcto? &#191;Si vale la pena?</strong>
+							<br />
+						Cada caso es diferente, as&#237; que no ser&#237;a posible dar un criterio que les sirva a todos. Pero si hay algunas consideraciones que pueden tomar en cuenta al momento de tomar una decisi&#243;n.  Te debes preguntar:</p>
+
+					<ol>
+						<li>&#191;Cu&#225;nto me va costar mi nueva educaci&#243;n en comparaci&#243;n al posible aumento de salario que gane despu&#233;s de obtener mi t&#237;tulo?></li>
+
+						<li>&#191;C&#243;mo le va impactar esto a mi familia? &#191;Al corto plazo? &#191;Al largo plazo?</li>
+
+						<li>&#191;Cu&#225;les trabajos est&#225;n disponibles para aquellos con este t&#237;tulo?</li>
+
+						<li>&#191;Qu&#233; provecho le sacar&#237;a a mi educaci&#243;n comparada con la experiencia que ya tengo en mi carrera o negocio?</li>
+
+					</ol>
+
+						<p>En s&#237;, lo m&#225;s importante es que tengan muy claro en mente sus prioridades.  El regresar al colegio es una gran meta, una que probablemente le requerir&#225; sacrificar prioridades menos importantes.</p>
+
+						<p><strong>Ahora, los estudiantes en las universidades son por la mayor parte j&#243;venes de 18 a 22 a&#241;os de edad.  &#191;No se sentir&#237;a uno de mayor edad un poco extra&#241;o al estar rodeado de compa&#241;eros mucho menor de edad?</strong>
+						<br />
+					
+						Esto es un sentimiento muy com&#250;n, pero no refleja la realidad.  Hay m&#225;s y m&#225;s adultos regresando al colegio.  De acuerdo al Censo, al partir del 2007, los adultos mayores de 37 anos formaron 15% de la educaci&#243;n universitaria. Y tomen en cuenta que los profesores aprecian a los estudiantes que tienen mucha experiencia, ya que pueden compartir esas experiencias con otros estudiantes que solamente han estado expuestos a la teor&#237;a en la clase.  Es m&#225;s, esta vez le sacaras m&#225;s provecho, ya que tienes en mente el provecho que le quieres sacar al t&#237;tulo.</p>
+
+						<p><strong>&#191;Para aquellos que no tengan manera de financiar los costos de la universidad, que recursos pueden usar para ayudarles cumplir sus sue&#241;os?</strong>
+						<br />
+						Cr&#233;elo o no, hay bastantes recursos disponibles.  Les voy a sugerir tres que siento que son los m&#225;s alcanzables:</p>
+
+					<ol>
+					
+						<li>Puedes que califiques para ayuda financiera del gobierno, aun si no est&#225;s en el colegio &#8220;full time.&#8221;  Para ver si calificas, busque en <a href="www.FederalStudentAid.ed.gov">www.FederalStudentAid.ed.gov</a>.</li>
+
+						<li>Para poder analizar una variedad de becas y programas de financiamiento de matricula pueden visitar Fast Web Scholarship Search &#8211; <a href="www.fastweb.com">www.fastweb.com</a></li>
+
+						<li>Tambi&#233;n hay recursos y becas espec&#237;ficamente para los hispanos &#8211; para mas informaci&#243;n pueden visitar al Hispanic Scholarship Fund &#8211; <a href="www.hsf.net">www.hsf.net</a></li>
+
+						<li>Tambi&#233;n es posible que consigas la asistencia de tu empleador.  Si tu educaci&#243;n es relevante a tu empleo, tu compa&#241;&#237;a puede tener un programa de matriculaci&#243;n donde te reembolsan todos o parte de los gastos.</li>
+
+					</ol>
+						<p>Tambi&#233;n acu&#233;rdense, que el regresar a la universidad no es la &#250;nica opci&#243;n.  Hoy en d&#237;a, hay cursos que pueden tomar a trav&#233;s de la computadora a las horas del d&#237;a que les convenga.  Muchas universidades tambi&#233;n ofrecen cursos por las noches o los fines de semana.  Esto les permite mantener su empleo y terminar un titulo o curso de modo flexible.</p>
+					
+					</div>
+					<!-- END Resource 03.17.11 -->
+					
+				<div class="resource">
+					
+					<a name="030311"></a>	
+					<h1>Cambiar de Carrera - March 3, 2011</h1>
+					
+					<p><strong>Cambiar de trabajo es siempre estresante, pero junto con la incertidumbre econ&#243;mica que existe hoy en d&#237;a, puede ser algo aterrador!  A pesar de esto, hay muchas personas que se encuentran en una situaci&#243;n en la cual tienen que contemplar cambiar no solo de trabajo, pero quiz&#225;s cambiar de carrera.</strong></p>
+
+					<p><strong>&#191;Es ahora un buen momento para cambiar de carrera?</strong></p>
+					<p>La respuesta a esa pregunta es si y no&#8230;depende.  Primero, tenemos que estar consientes de que en estos momentos&#8230;</p>
+
+
+					<ul>
+						<li>hay menos trabajos disponibles que en otros tiempos</li>
+
+						<li>empleadores no est&#225;n dispuestos a tomar riesgos (as&#237; que prefieren &#8220;ir a lo seguro&#8221; y contratar a personas con experiencia)</li>
+
+						<li>hay muchas personas calificadas buscando trabajo</li>
+					</ul>
+
+
+
+					<p>Por estas razones la respuesta seria no&#8230; no es un buen tiempo para cambiar de carrera.</p>
+
+					<p>Pero tambi&#233;n existen otros factores que tenemos que considerar&#8230;</p>
+
+					<ul>
+						<li>si su trabajo es en una industria que est&#225; en declive</li>
+
+						<li>si est&#225; desempleado y no logra encontrar trabajo en su industria</li>
+
+						<li>si su trabajo le causa demasiado estr&#233;s y le afecta su salud</li>
+					</ul>
+
+
+					<p>Entonces le digo que si es momento de cambiar de carrera.</p>
+
+					<p><strong>&#191;Si uno decide que su mejor opci&#243;n es cambiar de carrera, que puede hacer para facilitar el proceso?</strong></p>
+					<p>Cambiar de carrera durante una recesi&#243;n no es f&#225;cil-pero no es imposible&#8230;</p>
+
+
+					<ul>
+						<li>Identificar qu&#233; sector le interesa y estudie si ese sector es uno que vaya a tener crecimiento</li>
+						<li>Hacer contactos a trav&#233;s de redes sociales y profesionales</li>
+					</ul>
+
+					<p>Para m&#225;s informaci&#243;n sobre este tema, leyese estos tres art&#237;culos:</p>
+					<ul class="hotResourcesList">
+						<li>
+							<a href="http://education.yahoo.net/articles/quick_career_changes.htm?utm_medium=website&utm_source=mgtunon">Quick Career Changes for 2011</a>
+						</li>
+						<li>
+							<a href="http://msn.careerbuilder.com/Article/MSN-2457-Job-Info-and-Trends-Hot-Fields-for-Hiring-in-2011?utm_medium=website&utm_source=mgtunon">Hot Fields for Hiring in 2011</a>
+						</li>
+						<li>
+							<a href="http://money.usnews.com/money/careers/articles/2010/12/06/the-50-best-careers-of-2011?PageNr=3&utm_medium=website&utm_source=mgtunon">The 50 Best Careers of 2011</a>
+						</li>
+						
+					</ul>
+				</div>
+<!-- END Resource 03.03.11 -->
+					
+				</section>
+				<!-- end #mgt_column -->
+			
+			</div>
+			<!-- end #page_main_left -->
+			
+
+			<?php get_sidebar('main'); ?>
+			<!-- END sidebar -->
+			
+		</div>
+		<!-- end #page_main -->
+	
+	
+<?php get_footer(); ?>
