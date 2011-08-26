ad14d36d8873eb1c67077915f4351e5caa13b1e4
Daniel Campo <accounts@epiksolcreative.com>
2011-08-26 16:50:50 -0400

    Removed local fallback for jQuery to not conflict with W3TC minify.

--- application/wp-content/themes/mgtunon/footer.php
+++ application/wp-content/themes/mgtunon/footer.php
@@ -9,7 +9,6 @@
 	<!-- end #page_container -->
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="library/js/libs/jquery-1.6.1.min.js">\x3C/script>')</script>	
 	<script src="<?php bloginfo('template_directory') ?>/library/js/plugins.js"></script>
 	<script src="<?php bloginfo('template_directory') ?>/library/js/script.js"></script>
 	<!--[if lt IE 7 ]>
